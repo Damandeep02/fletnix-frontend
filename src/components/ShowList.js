15ca75a7e76ba139302359fb06940d867916842c
Damandeep Singh <dmnsingh007@gmail.com>
2023-09-08 17:46:11 +0530

    Added ascending, descedninf filter|Daman

--- src/components/ShowList.js
+++ src/components/ShowList.js
@@ -5,12 +5,13 @@ import SearchBar from './Common/SearchBar';
 import Filter from './Common/Filter';
 import Pagination from './Common/Pagination';
 import { Link } from 'react-router-dom';
-import { Button, TextField, Container, Typography, Grid, MenuItem} from '@mui/material';
+import { Button, TextField, Container, Typography, Grid, MenuItem } from '@mui/material';
 import StarIcon from '@mui/icons-material/Star';
 import SearchIcon from '@mui/icons-material/Search';
 import './ShowList.css';
-import FletNix from '../FletNix-Movies-9-5-2023.png'
-import Logo from '../FletNix-9-5-2023.png'
+import FletNix from '../FletNix-Movies-9-5-2023.png';
+import Logo from '../FletNix-9-5-2023.png';
+
 const ShowList = () => {
   const [movies, setMovies] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
@@ -18,6 +19,7 @@ const ShowList = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [filterType, setFilterType] = useState('all');
   const userAge = parseInt(localStorage.getItem('age'));
+  const [sortingOption, setSortingOption] = useState('asc');
 
   const history = useNavigate();
 
@@ -32,21 +34,33 @@ const ShowList = () => {
             filterType: filterType,
           },
         });
-  
+
+        const filteredMovies =
+          userAge < 18 &&
+          (filterType === 'all' || filterType === 'Movie' || filterType === 'TV Show')
+            ? response.data.shows.filter((movie) => movie.rating !== 'R')
+            : response.data.shows;
+
+            const sortedMovies = filteredMovies.sort((a, b) => {
+              const dateA = new Date(a.date_added);
+              const dateB = new Date(b.date_added);
         
-        const filteredMovies = userAge < 18 && (filterType === 'all' || filterType === 'Movie' || filterType === 'TV Show' )
-          ? response.data.shows.filter(movie => movie.rating !== 'R')
-          : response.data.shows;
-  
-        setMovies(filteredMovies);
-        setTotalPages(response.data.totalPages);
+              if (sortingOption === 'asc') {
+                return dateA - dateB;
+              } else if (sortingOption === 'desc') {
+                return dateB - dateA;
+              }
+            });
+        
+            setMovies(sortedMovies);
+            setTotalPages(response.data.totalPages);
       } catch (error) {
         console.error('Error fetching shows:', error);
       }
     };
-  
+
     fetchShows();
-  }, [currentPage, searchTerm, filterType, userAge]);
+  }, [currentPage, searchTerm, filterType, userAge, sortingOption]);
 
   const handlePageChange = (page) => {
     setCurrentPage(page);
@@ -57,12 +71,15 @@ const ShowList = () => {
     setCurrentPage(1);
   };
 
+
   const handleFilterChange = (type) => {
     setFilterType(type);
     setCurrentPage(1);
   };
+
   const handleLogout = () => {
     localStorage.removeItem('age');
+    localStorage.removeItem('token');
 
     history('/');
   };
@@ -70,17 +87,21 @@ const ShowList = () => {
   const calculateMovieNumber = (page, index) => {
     return (page - 1) * 15 + index + 1;
   };
-  
+
+  const handleSortingOptionChange = (option) => {
+    setSortingOption(option);
+  };
+
   const filteredMovies = movies.filter((movie) => {
     if (filterType === 'all') {
-      return true; 
+      return true;
     } else {
-      return movie.type === filterType; 
+      return movie.type === filterType;
     }
   });
+
   return (
-    <div className="show-list-container" >
-      
+    <div className="show-list-container">
       <Button
         variant="contained"
         color="secondary"
@@ -92,32 +113,42 @@ const ShowList = () => {
       <img src={Logo} alt="FletNix Logo" className="logo" />
       <h2 className="show-list-title">LIST OF GREAT MOVIES AND SHOWS</h2>
       <div className="search-container">
-  <TextField
-    label="Search"
-    variant="outlined"
-    fullWidth
-    value={searchTerm}
-    onChange={(e) => handleSearch(e.target.value)}
-    InputProps={{
-      endAdornment: <SearchIcon />,
-    }}
-    className="search-bar"
-  />
-  <TextField
-    select
-    label="Filter"
-    variant="outlined"
-    value={filterType}
-    style={{marginLeft: '8rem'}}
-    onChange={(e) => handleFilterChange(e.target.value)}
-    className="filter-dropdown"
-  >
-    <MenuItem value="all">All</MenuItem>
-    <MenuItem value="Movie">Movie</MenuItem>
-    <MenuItem value="TV Show">TV Show</MenuItem>
-  </TextField>
-</div>
-
+        <TextField
+          label="Search"
+          variant="outlined"
+          fullWidth
+          value={searchTerm}
+          onChange={(e) => handleSearch(e.target.value)}
+          InputProps={{
+            endAdornment: <SearchIcon />,
+          }}
+          className="search-bar"
+        />
+        <TextField
+          select
+          label="Filter"
+          variant="outlined"
+          value={filterType}
+          style={{ marginLeft: '8rem' }}
+          onChange={(e) => handleFilterChange(e.target.value)}
+          className="filter-dropdown"
+        >
+          <MenuItem value="all">All</MenuItem>
+          <MenuItem value="Movie">Movie</MenuItem>
+          <MenuItem value="TV Show">TV Show</MenuItem>
+        </TextField>
+        <TextField
+          select
+          label="Sort by Date Added"
+          variant="outlined"
+          value={sortingOption}
+          onChange={(e) => handleSortingOptionChange(e.target.value)}
+          className="sort-dropdown" 
+        >
+          <MenuItem value="asc">Ascending</MenuItem>
+          <MenuItem value="desc">Descending</MenuItem>
+        </TextField>
+      </div>
       <ul className="show-list">
         {filteredMovies.map((movie, index) => (
           <li key={movie.show_id} className="movie-item">
@@ -131,7 +162,6 @@ const ShowList = () => {
               <Link to={`/shows/${movie._id}`} className="movie-link">
                 {movie.title}
               </Link>
-              
               <div className="movie-rating">
                 <StarIcon fontSize="small" color="primary" /> {movie.rating}
               </div>
